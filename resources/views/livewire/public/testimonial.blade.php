cf339a3235b87206140b52f0265ab9a937d0805e
Saumy Anand <samcool3203@gmail.com>
2025-10-30 20:50:01 +0530

    testimonial blade added

--- resources/views/livewire/public/testimonial.blade.php
+++ resources/views/livewire/public/testimonial.blade.php
@@ -1,15 +1,15 @@
 <div>
       <main>
         <!--page-title-area start-->
-        <section class="page-title-area page-area-two page-t-height pb-155" style="background-image: url(assets/img/page-title/tm1.jpg)">
-            <div class="page-title-img" style="background-image: url(assets/img/page-title/tm1.jpg);">
+        <section class="page-title-area page-area-two page-t-height pb-155" style="background-image: url({{asset('assets/img/page-title/tm1.jpg')}})">
+            <div class="page-title-img" style="background-image: url({{asset('assets/img/page-title/tm1.jpg')}});">
                  <h1 class="title-text d-none d-lg-inline-block">Testimonial</h1>
             </div>
             <div class="container">
                 <div class="row">
                     <div class="col-xl-6 col-lg-12">
                         <div class="page-title-wrapper page-wrapper-team page-wrapper-white pt-240 pt-md-200 pt-xs-150">
-                            <h1 class="page-title mb-60">Over <span class="round-line">300k+</span> clients love our work. </h1>
+                            <h1 class="page-title mb-60">Over <span class="round-line">500+</span> clients love our work. </h1>
                             <h4 class="sub-title mb-35 pr-80 pr-xs-0">Employees need to realize the importance of working well with their teammates when coming into a new job or an existing one. A team player is more valuable.</h4>
                         </div>
                     </div>
@@ -20,12 +20,12 @@
         <!--page-title-area end-->
         <!--client-feedback-area start-->
         <section class="client-feedback-area cf-area-three pos-rel pt-100 pb-100 pt-md-85 pb-mb-60 pt-xs-85 pb-xs-100">
-            <img class="test test_01 d-none d-lg-inline-block" src="assets/img/testimonial/10.png" alt="">
-            <img class="test test_02 d-none d-lg-inline-block" src="assets/img/testimonial/11.png" alt="">
-            <img class="test test_03 d-none d-lg-inline-block" src="assets/img/testimonial/12.png" alt="">
-            <img class="test test_04 d-none d-lg-inline-block" src="assets/img/testimonial/13.png" alt="">
-            <img class="test test_05 d-none d-lg-inline-block" src="assets/img/testimonial/14.png" alt="">
-            <img class="test test_06 d-none d-lg-inline-block" src="assets/img/testimonial/15.png" alt="">
+            <img class="test test_01 d-none d-lg-inline-block" src="{{ asset('assets/img/testimonial/10.png') }}" alt="">
+            <img class="test test_02 d-none d-lg-inline-block" src="{{ asset('assets/img/testimonial/11.png') }}" alt="">
+            <img class="test test_03 d-none d-lg-inline-block" src="{{ asset('assets/img/testimonial/12.png') }}" alt="">
+            <img class="test test_04 d-none d-lg-inline-block" src="{{ asset('assets/img/testimonial/10.png') }}" alt="">
+            <img class="test test_05 d-none d-lg-inline-block" src="{{ asset('assets/img/testimonial/14.png') }}" alt="">
+            <img class="test test_06 d-none d-lg-inline-block" src="{{ asset('assets/img/testimonial/15.png') }}" alt="">
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-lg-6">
@@ -39,39 +39,40 @@
                 <div class="row no-gutters justify-content-center">
                     <div class="col-lg-9">
                         <div class="feedback-active4 owl-carousel">
-                            <div class="feedback-item-wrapper">
-                                <div class="feedback-box fb-box-3 text-center">
-                                    <div class="quote-icon">
-                                        <img src="{{ asset('assets/img/icon/quote-gray.svg') }}" alt="">
+                            @forelse($testimonials as $testimonial)
+                                @php
+                                    $translation = $testimonial->translation;
+                                @endphp
+                                @if($translation)
+                                    <div class="feedback-item-wrapper">
+                                        <div class="feedback-box fb-box-3 text-center">
+                                            <div class="quote-icon">
+                                                <img src="{{ asset('assets/img/icon/quote-gray.svg') }}" alt="">
+                                            </div>
+                                            <h4 class="sub-title mb-25">{{ Str::limit($translation->content, 150) }}</h4>
+                                            <h5 class="mb-10">{{ Str::limit($testimonial->name, 30) }}</h5>
+                                            @if($translation->position && $translation->company)
+                                                <h6>{{ Str::limit($translation->position, 25) }}, {{ Str::limit($translation->company, 25) }}.</h6>
+                                            @elseif($translation->position)
+                                                <h6>{{ Str::limit($translation->position, 50) }}.</h6>
+                                            @elseif($translation->company)
+                                                <h6>{{ Str::limit($translation->company, 50) }}.</h6>
+                                            @endif
+                                        </div>
                                     </div>
-                                    <h4 class="sub-title mb-25">We’v agents around occae cat the country, Find agents your neighborhood.Lorem ipsum dolor sit amet consectetur, omnis. voluptate velit esse cillum dolore eu fugiat nulla</h4>
-                                    <h5 class="mb-10">Rashed Ka</h5>
-                                    <h6>Senior Designer, Squre.</h6>
-                                </div>
-                            </div>
-                            <div class="feedback-item-wrapper">
-                                <div class="feedback-box fb-box-3 text-center">
-                                    <div class="quote-icon">
-                                        <img src="{{ asset('assets/img/icon/quote-gray.svg') }}" alt="">
+                                @endif
+                            @empty
+                                <div class="feedback-item-wrapper">
+                                    <div class="feedback-box fb-box-3 text-center">
+                                        <div class="quote-icon">
+                                            <img src="{{ asset('assets/img/icon/quote-gray.svg') }}" alt="">
+                                        </div>
+                                        <h4 class="sub-title mb-25">No testimonials available at the moment.</h4>
+                                        <h5 class="mb-10">-</h5>
+                                        <h6>-</h6>
                                     </div>
-                                    <h4 class="sub-title mb-25">ommodo consequat. Duis aute irure dolor in reprehendert 
-                                        voluptate velit esse cillum dolore eu fugiat nulla
-                                        Excepteu sint occaecat cupidat non proident, sunt in culpa qui officia deserunt
-                                        mollit anim id est lrum.</h4>
-                                    <h5 class="mb-10">Hasan Mahmud</h5>
-                                    <h6>Senior Developer, Squre.</h6>
                                 </div>
-                            </div> 
-                            <div class="feedback-item-wrapper">
-                                <div class="feedback-box fb-box-3 text-center">
-                                    <div class="quote-icon">
-                                        <img src="{{ asset('assets/img/icon/quote-gray.svg') }}" alt="">
-                                    </div>
-                                    <h4 class="sub-title mb-25">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, cumque id! Nulla vero nam ipsa quae ut, ullam, ad repudiandae, tenetur facilis impedit velit maiores ipsum. Quos, sequi. Quod amet voluptatibus repellat veritatis.</h4>
-                                    <h5 class="mb-10">Rashed Ka</h5>
-                                    <h6>Senior Designer, Squre.</h6>
-                                </div>
-                            </div>
+                            @endforelse
                         </div>
                     </div>
                 </div>
@@ -80,7 +81,7 @@
         <!--client-feedback-area end-->
         <!--client-feedback-area start-->
         <section class="client-feedback-area testimonial-style-one pos-rel pt-100 pb-150 pt-md-85 pb-mb-60 pt-xs-85 pb-xs-60">
-            <div class="feedback-img-wrapper" style="background-image: url(assets/img/testimonial/01.jpg);">
+            <div class="feedback-img-wrapper" style="background-image: url({{asset('assets/img/testimonial/01.jpg')}});">
                  <h1 class="title-text">Testimonial's <br> Client</h1>
             </div>
             <div class="container">
@@ -88,7 +89,7 @@
                     <div class="col-xl-5 col-lg-8">
                         <div class="testimonial-text-wrapper mb-30 mb-md-0 mb-xs-0">
                             <div class="section-title text-center text-xl-left">
-                                <h3 class="mb-25"><span class="round-line">300k+</span> satisfied customer.</h3>
+                                <h3 class="mb-25"><span class="round-line">500+</span> satisfied customer.</h3>
                                 <h4 class="sub-title mb-15 mb-md-0 mb-xs-0">Things goes wrong have questions dsu.</h4>
                             </div>
                         </div>
@@ -97,83 +98,54 @@
             </div>
             <div class="container feedback-wrap">
                  <div class="feedback-active owl-carousel">
-                    <div class="feedback-item">
-
-                        <div class="feedback-inner-content">
-                            <div class="quote-icon mb-25">
-                                <img src="{{asset('assets/img/icon/icon10.svg')}}" alt="">
-                            </div>
-                            <h4 class="inner-text mb-35">We’v 9,000 agents around the country, Find agents near your neighborhood.</h4>
-                            <div class="client-box">
-                                <div class="client-img">
-                                    <img src="assets/img/testimonial/02.png" alt="">
+                    @forelse($testimonials as $testimonial)
+                        @php
+                            $translation = $testimonial->translation;
+                        @endphp
+                        @if($translation)
+                            <div class="feedback-item">
+                                <div class="feedback-inner-content">
+                                    <div class="quote-icon mb-25">
+                                        <img src="{{asset('assets/img/icon/icon10.svg')}}" alt="">
+                                    </div>
+                                    <h4 class="inner-text mb-35">{{ Str::limit($translation->content, 120) }}</h4>
+                                    <div class="client-box">
+                                        <div class="client-img">
+                                            @if($translation->photo)
+                                                <img height="30" width="30" style="border-radius:100%;" src="{{ asset('storage/' . $translation->photo) }}" alt="{{ Str::limit($testimonial->name, 20) }}">
+                                            @else
+                                                @if($translation->gender === 'female')
+                                                    <img  height="30" width="30" style="border-radius:100%;" src="{{ asset('assets/img/testimonial/female-avatar.jpg') }}" alt="{{ Str::limit($testimonial->name, 20) }}">
+                                                @elseif($translation->gender === 'male')
+                                                    <img  height="30" width="30" style="border-radius:100%;" src="{{ asset('assets/img/testimonial/male-avatar.png') }}" alt="{{ Str::limit($testimonial->name, 20) }}">
+                                                @else
+                                                    <img  height="30" width="30" style="border-radius:100%;" src="{{ asset('assets/img/testimonial/default-avatar.png') }}" alt="{{ Str::limit($testimonial->name, 20) }}">
+                                                @endif
+                                            @endif
+                                        </div>
+                                        <h5>{{ Str::limit($testimonial->name, 25) }}</h5>
+                                        <p>{{ Str::limit($translation->address ?? 'Customer', 30) }}</p>
+                                    </div>
                                 </div>
-                                <h5>Rashed Kabir</h5>
-                                <p>USA</p>
+                            </div>
+                        @endif
+                    @empty
+                        <div class="feedback-item">
+                            <div class="feedback-inner-content">
+                                <div class="quote-icon mb-25">
+                                    <img src="{{asset('assets/img/icon/icon10.svg')}}" alt="">
+                                </div>
+                                <h4 class="inner-text mb-35">No testimonials available at the moment.</h4>
+                                <div class="client-box">
+                                    <div class="client-img">
+                                        <img src="{{ asset('assets/img/testimonial/default-avatar.png') }}" alt="Default">
+                                    </div>
+                                    <h5>-</h5>
+                                    <p>-</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="feedback-item">
-
-                        <div class="feedback-inner-content">
-                            <div class="quote-icon mb-25">
-                                <img src="{{asset('assets/img/icon/icon10.svg')}}" alt="">
-                            </div>
-                            <h4 class="inner-text mb-35">We’v 9,000 agents around the country, Find agents near your neighborhood.</h4>
-                            <div class="client-box">
-                                <div class="client-img">
-                                    <img src="assets/img/testimonial/02.png" alt="">
-                                </div>
-                                <h5>Hasan Mahmud</h5>
-                                <p>USA</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="feedback-item">
-                        <div class="feedback-inner-content">
-                            <div class="quote-icon mb-25">
-                                <img src="{{asset('assets/img/icon/icon10.svg')}}" alt="">
-                            </div>
-                            <h4 class="inner-text mb-35">Lorem ipsum dolor sit amet consectetur, adipisicing elit. this omnis.</h4>
-                            <div class="client-box">
-                                <div class="client-img">
-                                    <img src="assets/img/testimonial/03.png" alt="">
-                                </div>
-                                <h5>Martin Jonas</h5>
-                                <p>USA</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="feedback-item">
-                        <div class="feedback-inner-content">
-                            <div class="quote-icon mb-25">
-                                <img src="{{asset('assets/img/icon/icon10.svg')}}" alt="">
-                            </div>
-                            <h4 class="inner-text mb-35">ipsum consectetur around the country, Find agents near your neighborhood.</h4>
-                            <div class="client-box">
-                                <div class="client-img">
-                                    <img src="assets/img/testimonial/03.png" alt="">
-                                </div>
-                                <h5>Martin Jonas</h5>
-                                <p>USA</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="feedback-item">
-                        <div class="feedback-inner-content">
-                            <div class="quote-icon mb-25">
-                                <img src="{{asset('assets/img/icon/icon10.svg')}}" alt="">
-                            </div>
-                            <h4 class="inner-text mb-35">We’v 9,000 agents around the country, Find agents near your neighborhood.</h4>
-                            <div class="client-box">
-                                <div class="client-img">
-                                    <img src="assets/img/testimonial/04.png" alt="">
-                                </div>
-                                <h5>Martin Jonas</h5>
-                                <p>USA</p>
-                            </div>
-                        </div>
-                    </div>
+                    @endforelse
                 </div>
             </div>
         </section>
@@ -186,8 +158,8 @@
                         <div class="col-xl-10">
                             <div class="subscribe-wrapper">
                                 <div class="section-title text-center">
-                                    <h3 class="mb-25">Ready to take plan? It’s just a matter of one <span class="round-line">clike</span></h3>
-                                    <h4 class="sub-title mb-50">Try it risk free — we don’t charge cancellation fees.</h4>
+                                    <h3 class="mb-25">Ready to take plan? It's just a matter of one <span class="round-line">clike</span></h3>
+                                    <h4 class="sub-title mb-50">Try it risk free — we don't charge cancellation fees.</h4>
                                     <a href="contact.html" class="theme_btn sub-btn">Get your free quote</a>
                                 </div>
                             </div>
